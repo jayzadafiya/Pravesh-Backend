383d7c38e04c2f86ff2aa2e203b6760f102b438f
Jay Zadafiya <zadafiyajay2@gmail.com>
2025-03-19 23:16:12 +0530

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
