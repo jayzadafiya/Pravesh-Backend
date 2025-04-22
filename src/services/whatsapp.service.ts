9f1b1293bc9e911db954f242995dcbe2396315d2
Jay Zadafiya <zadafiyajay2@gmail.com>
2025-04-22 21:27:32 +0530

    Whatsup image msg

--- src/services/whatsapp.service.ts
+++ src/services/whatsapp.service.ts
@@ -12,11 +12,37 @@ class whatsappService {
       {
         messaging_product: "whatsapp",
         recipient_type: "individual",
-        to: "91" + phone, // Use the actual phone number dynamically
+        to: "91" + phone,
         type: "text",
         text: {
           preview_url: false,
-          body: `Your OTP is: ${OTP}`, // Dynamic OTP message
+          body: `Your OTP is: ${OTP}`,
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  async sendQRMessage(phone: string, imageUrl: string, caption?: string) {
+    const API_KEY = process.env.WHATSAPP_API_KEY;
+    const whatsappAPI =
+      process.env.WHATSAPP_URL + process.env.PHONE_NUMBER_ID! + "/messages";
+
+    await axios.post(
+      whatsappAPI,
+      {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        to: "91" + phone,
+        type: "image",
+        image: {
+          link: imageUrl,
+          caption: caption || "Here is your QR code!",
         },
       },
       {
