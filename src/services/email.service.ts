0ddace8538930a4d81c7e8b45b864685e949dce1
Jay Zadafiya <zadafiyajay2@gmail.com>
2025-07-18 01:03:26 +0530

    update for pay tickets and fix bugs

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -2,6 +2,7 @@ import path from "path";
 import fs from "fs";
 import { transporter } from "../config/email.config";
 import { BadRequestException } from "../utils/exceptions";
+import moment from "moment";
 
 class emailService {
   sendAuthEmail = async (to: string, token: string) => {
@@ -21,6 +22,49 @@ class emailService {
         from: `"Prevesh Events" <${process.env.PRAVESH_INFO_EMAIL}>`,
         to,
         subject: "Your Authentication Token",
+        html: htmlTemplate,
+      });
+
+      console.log(`Message sent: ${info.messageId}`);
+    } catch (error: any) {
+      console.error(`Error sending email: ${error.message}`);
+      throw new BadRequestException(`Failed to send email: ${error.message}`);
+    }
+  };
+
+  sendTicketConfirmationEmail = async (data: any, user: any) => {
+    try {
+      const templatePath = path.join(
+        __dirname,
+        "../templates/ticket-confirmation.html"
+      );
+      let htmlTemplate = fs.readFileSync(templatePath, "utf8");
+
+      htmlTemplate = htmlTemplate.replace(
+        /{{eventMainBanner}}/g,
+        data?.eventMainBanner
+      );
+      htmlTemplate = htmlTemplate.replace(/{{eventName}}/g, data.eventName);
+      htmlTemplate = htmlTemplate.replace(
+        /{{eventDate}}/g,
+        moment(data.eventDate).format("MMMM Do YYYY")
+      );
+      htmlTemplate = htmlTemplate.replace(
+        /{{eventTime}}/g,
+        moment(data.eventDate).format("h:mm a")
+      );
+      htmlTemplate = htmlTemplate.replace(/{{venueName}}/g, data.venueName);
+      htmlTemplate = htmlTemplate.replace(/{{venueAddress}}/g, data.address);
+      htmlTemplate = htmlTemplate.replace(
+        /{{ticketCount}}/g,
+        data?.ticketCount || 0
+      );
+      htmlTemplate = htmlTemplate.replace(/{{qrCodeUrl}}/g, user.qrCode);
+
+      const info = await transporter.sendMail({
+        from: `"Prevesh Events" <${process.env.PRAVESH_INFO_EMAIL}>`,
+        to: user.email,
+        subject: "Your Ticket Confirmation",
         html: htmlTemplate,
       });
 
